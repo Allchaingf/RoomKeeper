b314e6e24161df8a016e505234fd84fb07ba654f
anton0801 <danilovanton724@gmail.com>
2026-06-01 20:14:06 +0200

    Initial Commit

---
+++ RoamKeeper/RoamKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoamKeeperApp.swift
+//  RoamKeeper
+//
+//  Created by Антон on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct RoamKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
